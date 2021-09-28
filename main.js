dca7c6a81631e92f9da77aa3caeb2844c8b6f907
kingster <forgotoffcom@gmail.com>
2021-09-28 17:09:17 +0800

    added buttons

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 // function computerPlay() generates "rock", "scissors" or "paper"
-// function oneRound(playerSelection, computerSelection) return string of "You lose! Paper beats Rock"
-//      parameters are case-insensitive
-// function game() to play oneRound() 5 times and report the winner at the end
-//      get user input with prompt
-//      get computer input with computerPlay()
-
-// returns string of either "rock", "scisors" or "paper"
+// function oneRound(e) is called when button is pressed
+// global var playerPoint and computerPoint keeps track of who won
+var playerPoint=0,computerPoint=0;
 function computerPlay() {
     let rsp = ["rock", "scissors", "paper"];
     let randnum = Math.floor(Math.random() * 3); // returns between 0-2
@@ -13,9 +9,9 @@ function computerPlay() {
     return rsp[randnum];
 }
 
-function oneRound(playerSelection, computerSelection) {
-    playerSelection=playerSelection.toLowerCase();
-    computerSelection=computerSelection.toLowerCase();
+function oneRound(e) {
+    let playerSelection=this.textContent.toLowerCase();
+    let computerSelection=computerPlay().toLowerCase();
     let rsp = ["rock", "scissors", "paper"];
 
     let playerScore, computerScore; //0-rock 1-scissors 2-paper [0 win 1, 1 win 2, 2 win 0]
@@ -29,29 +25,36 @@ function oneRound(playerSelection, computerSelection) {
     }
 
     if (playerScore == 2 && computerScore == 0) {
-        return 1;
+        playerPoint+=1;
     }
-    else if (playerScore<computerScore){
-        return 1;
+    else if (playerScore < computerScore) {
+        playerPoint+=1;
     }
-    else if (computerScore<playerScore){
-        return 0;
+    else if (computerScore < playerScore) {
+        computerPoint+=1;
     }
-    else if (playerScore==computerScore){
-        return 0.5;
+    else if (playerScore == computerScore) {
+        playerPoint+=0.5;
+        computerPoint+=0.5;
+    }
+    document.querySelector('#score').textContent=`Your score: ${playerPoint} Computer score: ${computerPoint}`;
+    if(playerPoint>=5){
+        alert("You won!");
+        playerPoint=0;
+        computerPoint=0;
+    }
+    if (computerPoint>=5){
+        alert("You lost!");
+        playerPoint=0;
+        computerPoint=0;
     }
 }
 
-function game(){
-    let playerScore=0,computerScore=0;
-    for(let i=0;i<5;i++){
-        userInput=window.prompt("Please enter your input", "scissors")
-        playerScore+=oneRound(userInput,computerPlay());
-        computerScore=5-playerScore;
-    }
-    
-    console.log("Your Score: " + playerScore + "\nComputer Score: " + computerScore);
+function game() {
+    let playerScore = 0, computerScore = 0;
+    const buttons=Array.from(document.querySelectorAll('.rsp-btn'));
+    //console.log(buttons);
+    buttons.forEach(button => button.addEventListener('click',oneRound));
 }
 game();
-
 
